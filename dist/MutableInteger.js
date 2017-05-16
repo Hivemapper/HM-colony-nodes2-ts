4b53034341bc771d4566e37fb0aa6d479e35fe8d
Dane Pieri <danepieri@Danes-Powerbook-G4.local>
2017-05-16 16:35:14 -0700

    Just ship with the compiled code

---
+++ dist/MutableInteger.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var MutableInteger = (function () {
+    function MutableInteger(val) {
+        this.val = val;
+    }
+    return MutableInteger;
+}());
+exports.MutableInteger = MutableInteger;
+//# sourceMappingURL=MutableInteger.js.map
